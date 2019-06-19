e35185eb900d99a3a62a29a12530e4a911bbfd29
davidyeshi <37969473+davidyeshi@users.noreply.github.com>
2019-06-19 11:27:39 +1000

    Add files via upload

---
+++ precache-manifest.95a71da4bff0525be1ce899449e2db6c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db0d61bebfd37b3128fdaa7e293c453",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0c71012ba387a546511",
+    "url": "/static/css/main.5f36f7ae.chunk.css"
+  },
+  {
+    "revision": "9a3644f5b2494ec51ed2",
+    "url": "/static/js/2.85726d8d.chunk.js"
+  },
+  {
+    "revision": "b0c71012ba387a546511",
+    "url": "/static/js/main.879b7427.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c3a535a3505f263c54b237c660f4a1ba",
+    "url": "/static/media/australia.c3a535a3.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "74e78299f17bf754e44890e638b534b6",
+    "url": "/static/media/recipes-2.74e78299.png"
+  },
+  {
+    "revision": "d5ca37167a11b7b498e9a358b8ec0bb2",
+    "url": "/static/media/self.d5ca3716.jpeg"
+  }
+]);
